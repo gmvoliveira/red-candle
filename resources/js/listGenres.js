3ce7f9e6548134cf16844053b0910af87126a4e7
Gil Oliveira <gil.oliveira@movements.nl>
2020-04-22 17:47:06 -0300

    Add image and message when no album is selected

--- resources/js/listGenres.js
+++ resources/js/listGenres.js
@@ -1,6 +1,8 @@
 import GetGenres from './getGenres.js'
 
 const target = document.getElementById('genrelist')
+const targetEmpty = document.getElementById('genrelist-empty')
+const targetEmptyMemory = targetEmpty
 
 const renderGenre = (genre, weight) => {
     const template = `<div class="list-item">${genre}</div>`
@@ -10,7 +12,13 @@ const renderGenre = (genre, weight) => {
 const ListGenres = async query => {
     const genres = await GetGenres(query)
     if (!genres) return false
+    
+    // Empty genres
     target.innerHTML = null
+
+    targetEmpty.remove()
+
+    // Render new genres
     for (let [genre, weight] of Object.entries(genres)) {
         renderGenre(genre, weight)
     }
