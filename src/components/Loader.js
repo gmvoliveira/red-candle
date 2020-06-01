a45441fe574087ed6e0844c1b8b8f70c1d0aa2c6
Gil Oliveira <gil.oliveira@movements.nl>
2020-06-01 10:10:44 +0200

    Add loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = () => {
+    return (
+        <div className="loader">
+            <div className="ui big text loader">Loading...</div>
+        </div>
+    );
+};
+  
+  export default Loader;
